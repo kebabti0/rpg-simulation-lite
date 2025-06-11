1b5ae0d150f19306161256769b01b4bdbd57d46f
kebabti0 <yusuf080206@hotmail.com>
2025-06-11 19:38:56 +0200

    Enemy-Item change
    I gave every enemy only 1 item each, so they don't drag the game out much as the game is only meant to be a small RPG Simulation

--- enemies.py
+++ enemies.py
@@ -16,7 +16,7 @@ enemies = {
         "defense": 6,
         "speed": 6,
         "gold_drop": (75, 100),
-        "inventory": {"Small Potion": 1, "Power Elixir": 2, "Haste Potion": 1, "Hexdust": 2} # Early game enemy, more offensive and can boost speed, aggressive/burst
+        "inventory": {"Small Potion": 1, "Power Elixir": 1, "Haste Potion": 1, "Hexdust": 1} # Early game enemy, more offensive and can boost speed, aggressive/burst
     }, # Buff attack early, use Haste Potion if slower than player, heal if low, debuff if facing a Warrior
 
     "Ghost": {
@@ -25,7 +25,7 @@ enemies = {
         "defense": 6,
         "speed": 11,
         "gold_drop": (100, 125),
-        "inventory": {"Medium Potion": 1, "Hexdust": 1, "Shadow Veil": 2, "Sticky Web": 2} # Early game enemy, faster and can debuff attack and defense, glass cannon
+        "inventory": {"Medium Potion": 1, "Hexdust": 1, "Shadow Veil": 1, "Sticky Web": 1} # Early game enemy, faster and can debuff attack and defense, glass cannon
     }, # Debuff player's attack & defense first, heal if low, attack especially if player already debuffed
 
     "Chimaera": {
@@ -34,7 +34,7 @@ enemies = {
         "defense": 9,
         "speed": 7,
         "gold_drop": (125, 150),
-        "inventory": {"Medium Potion": 1,"Haste Potion": 2, "Sticky Web": 2, "Power Elixir": 2} # Mid game enemy, all rounder, acts accordingly 
+        "inventory": {"Medium Potion": 1,"Haste Potion": 1, "Sticky Web": 1, "Power Elixir": 1} # Mid game enemy, all rounder, acts accordingly 
     }, # Use Hase Potion if slower, Sticky web if faster or Hunter, heal if low, buff / attack otherwise
 
     "Sorcerer": {
@@ -43,7 +43,7 @@ enemies = {
         "defense": 5,
         "speed": 9,
         "gold_drop": (150, 175),
-        "inventory": {"Fire Scroll": 2, "Frost Shard": 2, "Cursed Ash": 2, "Medium Potion": 2} # Mid game enemy, uses special items, sets their burst up with debuffs
+        "inventory": {"Fire Scroll": 1, "Frost Shard": 1, "Cursed Ash": 1, "Medium Potion": 1} # Mid game enemy, uses special items, sets their burst up with debuffs
     }, # Open with Cursed Ash or Frost Shard against strong/fast players, use Fire Scroll for burst, heal if low
     "Golem": {
         "health": 200,
@@ -51,7 +51,7 @@ enemies = {
         "defense": 18,
         "speed": 4,
         "gold_drop": (175, 200),
-        "inventory": {"Stonewall Brew": 2, "Lightning Elixir": 2, "Fury Draught": 2, "Armour Piercer": 2, "Medium Potion": 2} # Mid game enemy, more defensive and can boost attack, tanky
+        "inventory": {"Stonewall Brew": 1, "Lightning Elixir": 1, "Fury Draught": 1, "Armour Piercer": 1, "Medium Potion": 1} # Mid game enemy, more defensive and can boost attack, tanky
     }, # Use Stonewall if defense is lower than player's attack, Fury Draught for burst, Lightning Elixir if player outspeeds, heal if low
 
     "Knight Aberrant": {
@@ -60,7 +60,7 @@ enemies = {
         "defense": 20,
         "speed": 6,
         "gold_drop": (200, 225),
-        "inventory": {"Titan's Brew": 2, "Armour Piercer": 2, "Lightning Elixir": 2, "Fury Draught": 2} # Late game enemy, has 75% block like warrior, is very tanky and sets up burst with pierce and buff
+        "inventory": {"Titan's Brew": 1, "Armour Piercer": 1, "Lightning Elixir": 1, "Fury Draught": 1} # Late game enemy, has 75% block like warrior, is very tanky and sets up burst with pierce and buff
     }, # Use Titan's Brew early, Armour Piercer against Warriors or tanky players, Lightning Elixir if slower, heal if low, defensive
 
     "Dragon": {
@@ -69,7 +69,7 @@ enemies = {
         "defense": 14,
         "speed": 10,
         "gold_drop": (225, 250),
-        "inventory": {"Mega Elixir": 2, "Bullhorn Brew": 2, "Lightning Elixir": 2, "Titan's Brew": 2, "Fire Scroll": 2} # Final boss, has the best items and stats, very aggressive
+        "inventory": {"Mega Elixir": 1, "Bullhorn Brew": 1, "Lightning Elixir": 1, "Titan's Brew": 1, "Fire Scroll": 1} # Final boss, has the best items and stats, very aggressive
     } # Buff up First (Bullhorn, Titan's), then lightning if slower, Mega Elixir if low, attack or Fire Scroll for burst
 
 }
